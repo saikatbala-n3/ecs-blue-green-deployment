001e286819414016c6461da6a23b09d3ea7050c5
saikatbala-n3 <saikatbala3@gmail.com>
2026-04-03 14:29:10 +0530

    add Terraform compute resources (ECS)

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -67,3 +67,35 @@ resource "aws_ecs_task_definition" "app" {
         }
     }])
 }
+
+resource "aws_ecs_service" "app" {
+    name = "${local.name}-service"
+    cluster = aws_ecs_cluster.main.id
+    task_definition = aws_ecs_task_definition.app.arn
+    desired_count = var.desired_count
+    launch_type = "FARGATE"
+
+    deployment_controller {
+      type = "CODE_DEPLOY"
+    }
+
+    network_configuration {
+        subnets = aws_subnet.private[*].id
+        security_groups = [aws_security_group.ecs_tasks.id]
+        assign_public_ip = false
+    }
+
+    load_balancer {
+      target_group_arn = aws_lb_target_group.blue.arn
+      container_name = "${local.name}-app"
+      container_port = var.container_port
+    }
+
+    lifecycle {
+      ignore_changes = [task_definition, load_balancer, desired_count]
+    }
+    depends_on = [aws_lb_listener.prod]
+    tags = {
+        Name = "${local.name}-service"
+    }
+}
